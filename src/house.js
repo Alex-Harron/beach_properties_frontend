e832da6a17412dfa38b5edde98bf05ab087f9212
Alex Harron <aharron.harron@gmail.com>
2021-06-25 10:12:03 -0400

    updating HTML

--- src/house.js
+++ src/house.js
@@ -25,9 +25,18 @@ class House {
 
     houseHTML(){
         this.element.innerHTML += `
-            <div>
-                <h3>${this.name}</h3>
-                <p>${this.description} - <img src= "${this.img_url}"></p>
+            <div class="w3-row-padding w3-padding-16">
+                <div class="w3-third w3-margin-bottom">
+                    <img src="${this.img_url}" style="width:100%;">
+                    <div class="w3-container w3-white">
+                        <h3>${this.name}</h3>
+                        <h6 class="w3-opacity">From $99</h6>
+                        <p>${this.description}</p>
+                        <p>15m<sup>2</sup></p>
+                        <p class="w3-large"><i class="fa fa-bath"></i> <i class="fa fa-phone"></i> <i class="fa fa-wifi"></i></p>
+                        <button class="w3-button w3-block w3-black w3-margin-bottom">Choose Room</button>
+                    </div>
+                </div>
             </div>
             `
         return this.element
